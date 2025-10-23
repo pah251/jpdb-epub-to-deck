3acec235412da7f42acd41c65b9f4412c5120576
Paul Hudson <paul.andrew.hudson@gmail.com>
2025-10-23 19:31:43 +0900

    Created pybind11 'add' function bridge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <pybind11/pybind11.h>
 
-int main() {
-    std::cout << "Hello CMake!" << std::endl;
-    return 0;
+int add(int a, int b)
+{
+    return a + b;
+}
+
+PYBIND11_MODULE(my_cpp_module, m)
+{
+    m.doc() = "A simple C++ module for Python";
+
+    m.def("add", &add, "A function to add 2 numbers but really quickly");
 }
